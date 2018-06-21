4d33bc7c13d755e80d6e80c57c8903e2e79a6cf1
cster143 <cster143@gmail.com>
2018-06-21 18:55:32 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -1,33 +1,33 @@
 
-# def list_artists
-#   library= {
-#   :taylor_swift => "Style",
-#   :silento => "Watch Me",
-#   :selena_gomez => "Good For You",
-#   :mark_ronson => "Uptown Funk!",
-#   :andy_grammer => "Honey I'm Good",
-#   :maroon_five => "Sugar"
-#   }
-#   library.each_key do |x|
-#     puts x
-#   end
-# end
-# list_artists
-#
-# def list_songs
-#   library= {
-#   :taylor_swift => "Style",
-#   :silento => "Watch Me",
-#   :selena_gomez => "Good For You",
-#   :mark_ronson => "Uptown Funk!",
-#   :andy_grammer => "Honey I'm Good",
-#   :maroon_five => "Sugar"
-#   }
-#   library.each_value do |y|
-#     puts y
-#   end
-# end
-# list_songs
+def list_artists
+  library= {
+  :taylor_swift => "Style",
+  :silento => "Watch Me",
+  :selena_gomez => "Good For You",
+  :mark_ronson => "Uptown Funk!",
+  :andy_grammer => "Honey I'm Good",
+  :maroon_five => "Sugar"
+  }
+  library.each_key do |x|
+    puts x
+  end
+end
+list_artists
+
+def list_songs
+  library= {
+  :taylor_swift => "Style",
+  :silento => "Watch Me",
+  :selena_gomez => "Good For You",
+  :mark_ronson => "Uptown Funk!",
+  :andy_grammer => "Honey I'm Good",
+  :maroon_five => "Sugar"
+  }
+  library.each_value do |y|
+    puts y
+  end
+end
+list_songs
 
 def delete_artist
   library= {
@@ -45,17 +45,17 @@ end
 delete_artist
 
 
-# def add_song_and_artist
-#   library= {
-#   :taylor_swift => "Style",
-#   :silento => "Watch Me",
-#   :selena_gomez => "Good For You",
-#   :mark_ronson => "Uptown Funk!",
-#   :andy_grammer => "Honey I'm Good",
-#   :maroon_five => "Sugar"
-#   }
-#   library[:ed_sheeran]="Thinking Out Loud"
-#   puts library
-# end
-#
-# add_song_and_artist
+def add_song_and_artist
+  library= {
+  :taylor_swift => "Style",
+  :silento => "Watch Me",
+  :selena_gomez => "Good For You",
+  :mark_ronson => "Uptown Funk!",
+  :andy_grammer => "Honey I'm Good",
+  :maroon_five => "Sugar"
+  }
+  library[:ed_sheeran]="Thinking Out Loud"
+  puts library
+end
+
+add_song_and_artist
